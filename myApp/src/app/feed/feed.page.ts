1d1e8e743507935a0acea963a38a3eb506b11493
Renan Oliveira <c.renan.o@gmail.com>
2021-06-23 13:05:11 -0300

    Created function 'somaDoisNumero'

--- myApp/src/app/feed/feed.page.ts
+++ myApp/src/app/feed/feed.page.ts
@@ -5,11 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
 })
-export class FeedPage implements OnInit {
 
+
+
+export class FeedPage implements OnInit {
+  public nome_Usuario:string = "Renan Oliveira";
   constructor() { }
 
+  public somaDoisNumeros(num1:number, num2:number): void{
+	alert(num1 + num2);
+	}
+
   ngOnInit() {
+	
+  }
+  ionViewDidEnter(){
+	this.somaDoisNumeros(10, 99);
   }
 
 }
